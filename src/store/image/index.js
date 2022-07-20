4e190d8eab095e9a9f179c5e611726bd094404d1
linhaiyang <18606145168@163.com>
2022-07-21 01:29:35 +0800

    fix 镜像版本列表的显示

--- src/store/image/index.js
+++ src/store/image/index.js
@@ -1,43 +1,50 @@
-//示例:user组件的仓库
-import {
-  reqImagelIST,reqImageVersionlIST,reqImageOverview
-  //从api模块中引入请求  
-} from '@/api'
 //准备actions,用于响应组件中的动作
 const actions = {
   //通过api里面的接口函数发送请求,获取服务器的数据
-  async getImageOverview({
-    commit
-  }, 
-    {warehouseId,warehouseName,imageCatalogs}
-  ) {
-       commit('WAREHOUSEID', warehouseId)
-       commit('WAREHOUSENAME', warehouseName)
-       commit('IMAGECATALOGS', imageCatalogs)
-     
+  getImageOverview({ commit }, { warehouseId, warehouseName, imageCatalogs }) {
+    commit("WAREHOUSEID", warehouseId);
+    commit("WAREHOUSENAME", warehouseName);
+    commit("IMAGECATALOGS", imageCatalogs);
   },
-}
+  getImageList({ commit }, {imageList,totalCount,currPageNum}) {
+    commit("IMAGELIST", imageList);
+    commit("TOTALCOUNT", totalCount);
+    commit("CURRPAGENUM", currPageNum);
+  },
+};
 //准备mutations,用于操作数据--state
 const mutations = {
   WAREHOUSEID(state, warehouseId) {
-    state.warehouseId = warehouseId
+    state.warehouseId = warehouseId;
   },
   WAREHOUSENAME(state, warehouseName) {
-    state.warehouseName = warehouseName
+    state.warehouseName = warehouseName;
   },
   IMAGECATALOGS(state, imageCatalogs) {
-    state.imageCatalogs = imageCatalogs
+    state.imageCatalogs = imageCatalogs;
   },
-}
+  IMAGELIST(state, imageList) {
+    state.imageList = imageList;
+  },
+  TOTALCOUNT(state, totalCount) {
+    state.totalCount = totalCount;
+  },
+  CURRPAGENUM(state, currPageNum) {
+    state.currPageNum = currPageNum;
+  },
+};
 //准备state,用于存储数据,要先准备仓库categoryList,才能存储数据
 const state = {
-  warehouseId:0,
-  warehouseName:'',
-  imageCatalogs:[],
-}
+  warehouseId: 0,
+  warehouseName: "",
+  imageCatalogs: [],
+  imageList: [],
+  pageSize:10,
+  totalCount:'',
+};
 
 //用于简化数据
-const getters = {}
+const getters = {};
 
 //创建并暴露store
 export default {
@@ -45,5 +52,5 @@ export default {
   actions,
   mutations,
   state,
-  getters
-}
+  getters,
+};
